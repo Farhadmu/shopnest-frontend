d290edfe3805d857d933c2595fd4753112e09799
ab-bakkar71 <ab.bakkar420@gmail.com>
2026-08-27 16:00:57 +0600

    fix(auth): update baseURL fallback in auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,12 +2,12 @@ import { createAuthClient } from "better-auth/react";
 
 /**
  * Client-side Better Auth React Client Instance
- * Exports authentication hooks and actions for browser-side React components.
  */
 export const authClient = createAuthClient({
   baseURL:
-    process.env.NEXT_PUBLIC_APP_URL ||
-    (typeof window !== "undefined" ? window.location.origin : undefined),
+    typeof window !== "undefined"
+      ? window.location.origin
+      : process.env.NEXT_PUBLIC_APP_URL || "https://shopnest-frontend-six.vercel.app",
 });
 
-export const { useSession, signIn, signUp, signOut, } = authClient;
+export const { useSession, signIn, signUp, signOut } = authClient;
