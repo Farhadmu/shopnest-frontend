be940324b14c3b315af7d02db5aee52742b81214
Farhadmu <mi0223937@gmail.com>
2026-09-19 09:15:36 +0600

    feat(live-map): re-export RealStoreMarkerData and MapFilterOption from LiveDeliveryMap

--- src/components/delivery/LiveDeliveryMap.tsx
+++ src/components/delivery/LiveDeliveryMap.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
-import { GoogleDeliveryMap, GoogleDeliveryMapProps, FleetRiderMarkerData, MultiDeliveryItem } from "./GoogleDeliveryMap";
+import { GoogleDeliveryMap, GoogleDeliveryMapProps, FleetRiderMarkerData, MultiDeliveryItem, RealStoreMarkerData, MapFilterOption } from "./GoogleDeliveryMap";
 
 export { GoogleDeliveryMap };
-export type { FleetRiderMarkerData, MultiDeliveryItem };
+export type { FleetRiderMarkerData, MultiDeliveryItem, RealStoreMarkerData, MapFilterOption };
 
 export interface LiveDeliveryMapProps extends GoogleDeliveryMapProps {}
 
